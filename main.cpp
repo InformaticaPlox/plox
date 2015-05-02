f90dee89b9cebaa094b4572105057ba029102acb
JuanI <JuanI@Juani-Lap>
2015-05-02 18:10:04 -0500

    new commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.cpp
+ * Author: JuanI
+ *
+ * Created on 2 de mayo de 2015, 06:06 PM
+ */
+
+#include <cstdlib>
+#include <cstdio>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printf("it's not about the please, it's about sending a plox");
+
+    return 0;
+}
+
